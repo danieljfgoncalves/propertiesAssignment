39c9fabe9db4fada501c462d1f18c88c0476683e
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-13 09:58:22 -0400

    Initial Commit

---
+++ properties/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  properties
+//
+//  Created by Daniel Goncalves on 2015-04-13.
+//  Copyright (c) 2015 DG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
